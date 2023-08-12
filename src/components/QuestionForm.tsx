652d335a6dd2e139b4b64b56b822f39c07ac9165
musthofaajs <musthofaaaah@gmail.com>
2023-08-12 15:11:02 +0700

    Improve UI QuestionForm and add remove option button

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import {
   Button,
   FormControl,
+  IconButton,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from '@mui/material';
 import styled from 'styled-components';
+import { Delete } from '@mui/icons-material';
 
 interface Option {
   rule: string;
@@ -21,12 +23,24 @@ interface QuestionFormProps {
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  gap: 20px;
+`;
+
+const OptionContainer = styled.div`
+  display: flex;
   gap: 10px;
+  align-items: center;
+`;
+
+const StyledTextField = styled(TextField)`
+  flex: 1;
 `;
 
 const QuestionForm: React.FC<QuestionFormProps> = ({ onSubmit }) => {
   const [question, setQuestion] = useState('');
-  const [options, setOptions] = useState<Option[]>([{ rule: 'May Select', answer: '' }]);
+  const [options, setOptions] = useState<Option[]>([
+    { rule: 'May Select', answer: '' },
+  ]);
 
   const handleOptionChange = (
     index: number,
@@ -38,6 +52,20 @@ const QuestionForm: React.FC<QuestionFormProps> = ({ onSubmit }) => {
     setOptions(updatedOptions);
   };
 
+  const handleAddOption = () => {
+    if (options.length < 4) {
+      setOptions([...options, { rule: 'May Select', answer: '' }]);
+    }
+  };
+
+  const handleRemoveOption = (index: number) => {
+    if (options.length > 1) {
+      const updatedOptions = [...options];
+      updatedOptions.splice(index, 1);
+      setOptions(updatedOptions);
+    }
+  };
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     const formData = { question, options };
@@ -46,42 +74,53 @@ const QuestionForm: React.FC<QuestionFormProps> = ({ onSubmit }) => {
     setOptions([{ rule: 'May Select', answer: '' }]);
   };
 
-  const handleAddOption = () => {
-    setOptions([...options, { rule: 'May Select', answer: '' }]);
-  };
-
   return (
     <FormContainer onSubmit={handleSubmit}>
-      <TextField
+      <StyledTextField
         label="Question"
         variant="outlined"
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
         required
+        margin="dense"
       />
       {options.map((option, index) => (
-        <div key={index}>
+        <OptionContainer key={index}>
           <FormControl variant="outlined">
             <InputLabel>Options Rule</InputLabel>
             <Select
               value={option.rule}
-              onChange={(e) => handleOptionChange(index, 'rule', e.target.value as string)}
+              onChange={(e) =>
+                handleOptionChange(index, 'rule', e.target.value as string)
+              }
               label="Options Rule"
             >
               <MenuItem value="May Select">May Select</MenuItem>
               <MenuItem value="Must Select">Must Select</MenuItem>
             </Select>
           </FormControl>
-          <TextField
+          <StyledTextField
             label="Options Answer"
             variant="outlined"
             value={option.answer}
             onChange={(e) => handleOptionChange(index, 'answer', e.target.value)}
             required
+            fullWidth
+            margin="dense"
           />
-        </div>
+          {options.length > 1 && (
+            <IconButton onClick={() => handleRemoveOption(index)}>
+              <Delete />
+            </IconButton>
+          )}
+        </OptionContainer>
       ))}
-      <Button variant="outlined" color="primary" onClick={handleAddOption}>
+      <Button
+        type="button"
+        variant="outlined"
+        color="primary"
+        onClick={handleAddOption}
+      >
         Add Option
       </Button>
       <Button type="submit" variant="contained" color="primary">
